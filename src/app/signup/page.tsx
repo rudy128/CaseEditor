6eb5a799a6b41b315db6071035b597e5a2c88fb7
Rudy <prathamkamthan1306@gmail.com>
2024-05-12 01:12:05 +0530

    Basics Done

---
+++ src/app/signup/page.tsx
@@ -0,0 +1,29 @@
+import { ArrowRight } from 'lucide-react'
+import React from 'react'
+
+const page = () => {
+    return (
+        <div className='flex justify-center items-center my-auto'>
+            <div className='w-[70%] flex flex-col gap-y-6 justify-center items-center p-12 bg-white rounded-2xl border border-grey shadow-xl'>
+                <h1 className='text-3xl'>Signup</h1>
+                <form action="" className='w-full'>
+                    <div className='flex flex-col gap-y-3 my-4 w-full'>
+                        <label htmlFor="Your Name">Your Name:</label>
+                        <input type="text" placeholder='Your Name' required className='rounded-xl border border-grey p-4' />
+                    </div>
+                    <div className='flex flex-col gap-y-3 my-4 w-full'>
+                        <label htmlFor="Email ID">Email ID:</label>
+                        <input type="text" placeholder='Email ID' required className='rounded-xl border border-grey p-4' />
+                    </div>
+                    <div className='flex flex-col w-full gap-y-3'>
+                        <label htmlFor="Password">Password:</label>
+                        <input type="password" placeholder='Password' required className='rounded-xl border border-grey p-4' />
+                    </div>
+                    <button className='w-full my-4 py-4 text-white bg-green-600 hover:bg-green-500 flex justify-center items-center rounded-2xl'>Signup<ArrowRight className='ml-1.5 h-5 w-5' /></button>
+                </form>
+            </div>
+        </div>
+    )
+}
+
+export default page
